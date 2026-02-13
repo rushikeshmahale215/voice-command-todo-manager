1c2bfe9d314a5b718c0ce912b77181cb7ef994a5
rushikeshmahale215 <rushikeshmahale215@gmail.com>
2026-02-13 21:07:50 +0530

    cors improve

--- Backend/main.py
+++ Backend/main.py
@@ -15,11 +15,8 @@ app = FastAPI(title="VoiceTodo API")
 # =========================
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://localhost:5173",
-    ],
     allow_credentials=True,
+    allow_origins=["https://voice-command-todo-manager.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
